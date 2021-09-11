84ce8c1813ea8651cbae90df7c057bc385a45ea0
Huynh Dust <huynhdust0411@gmail.com>
2021-09-11 17:59:35 +0700

    upload 100%

---
+++ CT176_OOP/LTHDT/src/buoi2/PhanSo.java
@@ -0,0 +1,134 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class PhanSo {
+	int tu;
+	int mau;
+	public PhanSo() {
+	}
+	
+	public PhanSo(int tu, int mau) {
+		this.tu=tu;
+		this.mau=mau;
+	}
+//==============================DustHuyn==============================//
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Nhap gia tri cho phan so:");
+			System.out.print("Tu so:");
+			this.tu=sc.nextInt();
+			System.out.print("Mau so:");
+			this.mau=sc.nextInt();
+		} while(this.mau==0);
+		
+	}
+//==============================DustHuyn==============================//
+	public void chuanhoa(PhanSo x) {
+		if(x.tu!=0 && x.mau<0) {
+			x.mau=-x.mau;
+			x.tu=-x.tu;
+		}	
+	}
+	public void chuanhoa() {
+			if(this.tu!=0 && this.mau<0) {
+				this.mau=-this.mau;
+				this.tu=-this.tu;
+			}
+	}
+//==============================DustHuyn==============================//	
+	public void display() {
+		chuanhoa();
+		int mau = this.mau;
+		int tu  = this.tu;
+
+		if (tu==0){
+			System.out.println("0");
+		}else if (mau==1) {
+			System.out.println(tu);
+		}else
+			System.out.printf("%d/%d\n",tu,mau);
+	}
+//==============================DustHuyn==============================//
+	public void nghichDao() {
+		int temp=this.tu;
+		this.tu=this.mau;
+		this.mau=temp;
+	}
+//==============================DustHuyn==============================//
+	public PhanSo giatriNghichDao() {	
+		int tu  = this.mau;
+		int mau = this.tu;
+		PhanSo x = new PhanSo(tu,mau);
+		return x;
+	}
+//==============================DustHuyn==============================//
+	public float giatriThuc() {
+		float tu = this.tu;
+		float mau= this.mau;
+		return (tu/mau);
+	}
+//==============================DustHuyn==============================//
+	public boolean lonHon(PhanSo a) {
+		if(giatriThuc() > a.giatriThuc())
+			return true;
+		else return false;
+	}
+//==============================DustHuyn==============================//
+	public PhanSo cong(PhanSo a) {
+		PhanSo x = new PhanSo();
+		x.tu =this.tu;
+		x.mau=this.mau;
+		
+		quydongCungMau(x, a);
+		x.tu+=a.tu;
+		x = rutgon(x);
+		return x;
+	}
+//==============================DustHuyn==============================//
+	public PhanSo tru(PhanSo a) {
+		if(a.tu>0) a.tu=-a.tu;
+		else if(a.mau>0) a.mau=-a.mau;
+		return this.cong(a);
+	}
+//==============================DustHuyn==============================//
+	public PhanSo nhan(PhanSo a) {
+		int mau = this.mau*a.mau;
+		int tu  = this.tu*a.tu;
+		PhanSo x = new PhanSo(tu, mau);
+		return x;
+	}
+//==============================DustHuyn==============================//
+	public PhanSo chia(PhanSo a) {
+		a = a.giatriNghichDao();
+		return nhan(a);
+	}
+	
+//==============================DustHuyn==============================//	
+	public PhanSo rutgon(PhanSo x) {
+		int min = Math.abs(x.tu);
+		if(Math.abs(x.tu) > Math.abs(x.mau)) min=x.mau;
+		for(int i=min;i>1;i--) {
+			if(x.mau%i==0 && x.tu%i==0) {
+				x.tu/=i;
+				x.mau/=i;
+			}
+		}
+		return x;
+	}
+//==============================DustHuyn==============================//	
+	public void quydongCungMau(PhanSo x, PhanSo a) {
+		if(x.mau!=a.mau) {
+			int mau=x.mau;
+			x.mau*=a.mau;
+			x.tu *=a.mau;
+			a.tu*=mau;
+			a.mau*=mau;
+		}
+	}
+//==============================DustHuyn==============================//	
+	public static void main(String[] args) {
+	}
+
+}
---
+++ CT176_OOP/LTHDT/src/buoi2/SDPhanSo.java
@@ -0,0 +1,80 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class SDPhanSo {
+	//+Luu y, khi xai ham x.cong(y) se lam thay doi gia tri cua y, 
+	// do phai quy dong; nhung khong lam thay doi gia tri cua x,
+	// do da tao doi tuong khac. Co the fix lai truong hop nay.
+	//+Ham display chi hien thi nhung chua co rut gon, co the fix.
+	public static void main(String[] args) {
+		PhanSo a = new PhanSo(3,7);
+		PhanSo b = new PhanSo(4,9);
+		a.display();
+		b.display();
+//==============================DustHuyn==============================//
+		PhanSo x = new PhanSo();
+		PhanSo y = new PhanSo();
+		System.out.println("-Phan so x");
+		x.input();
+		System.out.println("-Phan so y");
+		y.input();
+//==============================DustHuyn==============================//
+		System.out.print("Phan So nghich dao cua x: ");
+		PhanSo x1 = new PhanSo();
+		x1 = x.giatriNghichDao();
+		x1.display();
+//==============================DustHuyn==============================//
+		System.out.print("x + y = ");
+		(x.cong(y)).display();
+//==============================DustHuyn==============================//
+		System.out.print("Nhap so phan so: ");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		PhanSo[] arr = new PhanSo[n+1];
+		for(int i=1;i<=n;i++) {
+			arr[i] = new PhanSo();
+			System.out.println("Phan so "+i+":");
+			arr[i].input(); 
+			System.out.println("/////////");
+		}
+		for(int i=1;i<=n;i++)
+			arr[i].display();
+//==============================DustHuyn==============================//
+		System.out.print("Sum:");
+		PhanSo Sum = new PhanSo(0,1);
+		for(int j=1;j<=n;j++) {
+			Sum = arr[j].cong(Sum);
+		}
+		Sum.display();
+		for(int j=1;j<=n;j++)
+			arr[j].display();
+		
+		System.out.print("Max:");
+		float max=Float.MIN_VALUE;
+		int check=0;
+		for(int k=1;k<=n;k++) {
+			if(max < arr[k].giatriThuc()) {
+				max = arr[k].giatriThuc();
+				check = k;
+			}
+		}
+		arr[check].display();
+//==============================DustHuyn==============================//
+		System.out.println("Sort:");
+		PhanSo temp = new PhanSo();
+		for(int i=1;i<n;i++) {
+			for(int j=i+1;j<=n;j++) {
+				if(arr[i].giatriThuc() > arr[j].giatriThuc()) {
+					temp   = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i=1;i<=n;i++) {
+			arr[i].display();
+		}
+		
+	}
+}
