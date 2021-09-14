3e0de1a3bbbeeae7d5c999dec8fdf1ad565a14af
Huynh Dust <huynhdust0411@gmail.com>
2021-09-14 13:35:33 +0700

    add file general linkedList

--- CT177_Cau truc du lieu/2_2_DS_LienKet/Setting_Linked_List.c
+++ CT177_Cau truc du lieu/2_2_DS_LienKet/Setting_Linked_List.c
@@ -21,3 +21,86 @@ List makenullList1(){
     (*L)->Next=NULL;
     return L;
 }
+
+Position first(List L){
+    return L;
+}
+
+Position endList(List L){
+    Position p = L;
+    while(p->Next!=NULL){
+        p=p->Next;
+    }
+    return p;
+}
+
+ElementType retrieve(Position P, List L){
+    if(P->Next!=NULL)
+        return P->Next->Element;
+}
+
+Position next(Position P, List L){
+    return P->Next;
+}
+
+Position Locate(ElementType X, List L){
+    Position P = first(L);
+    int Found = 0;
+    while((P != endList(L)) && (Found == 0 )){
+        if(retrieve(P,L) == X) Found = 1;
+        else P = next(P, L);
+    }
+    return P;
+}
+
+int emptyList(List L){
+    Position p = L;
+    return p->Next==NULL;
+}
+
+void Read_List(List *L){
+    Position P = first(*L);
+    int n,x;
+    printf("Nhap so luong danh sach: ");scanf("%d",&(n));
+    for(int i=0;i<n;i++){
+        scanf("%d",&x);
+        Insert_List(x,P,&L);
+        P=P->Next;
+    }
+}
+
+void Print_List(List L){
+    Position P=first(L);
+    while(P->Next!=NULL){
+        printf("%d",retrieve(P,L));
+        P=P->Next;
+    }
+}
+
+void Insert_List(ElementType X,Position P,List *L){
+    Position T = (struct Node *)malloc(sizeof(struct Node));
+    T->Element=X;
+    T->Next=P->Next;
+    P->Next=T;
+}
+
+void Delete_List(Position P, List *L){
+    Position T;
+    if (P->Next!=NULL){
+       T=P->Next;
+       P->Next=T->Next;
+       free(T);
+    }
+}
+
+void Delete_duplicate(List *L){
+   
+}
+
+void swap(Position a, Position b, List *L){
+
+}
+
+void Sort_List(List *L){
+
+}
